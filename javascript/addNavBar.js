fa7b6c6c47640043514cdb2532b6da604b60e4b6
Jesse Smith <jesse.etan.smith@gmail.com>
2015-07-28 15:21:19 -0400

    fixed nav bar js (and deleted code in index with navbar)

--- javascript/addNavBar.js
+++ javascript/addNavBar.js
@@ -1,27 +1,15 @@
 function addNavBar(){
-document.write('<div class="navigation"><ul class="sm sm-mint"><li><a href="#">Home</a></li><li><a href="#">User</a><ul><li><a href="#">Submit Research</a></li><li><a href="#">Edit Account</a></li></ul></li><li><a href="#">About</a><ul><li><a href="#">Our Mission</a></li><li><a href="#">Contact Us</a></li></ul></li></ul></div>');
+document.write('<div class="navigation">\
+  		<ul class="sm sm-mint">\
+  			<li><a href="index.html">Home</a></li>\
+			<li>\
+			 	User\
+			 	<ul>\
+			 		<li><a href="#">Submit Research</a></li>\
+			 		<li><a href="#">Edit Account</a></li>\
+			 	</ul>\
+			</li>\
+			<li><a href="about.html">About</a></li>\
+  		</ul>\
+  	</div>');
 }
-/* Javscript doesn't allow you to space out a string over multiple lines for some reason (that's the only time it's white space insensitive).
-If you want to edit the NavBar, edit this code, then paste it into a search bar to make it one line.
-
-	<div class="navigation">
-  		<ul class="sm sm-mint">
-  			<li><a href="#">Home</a></li>
-			<li>
-			 	<a href="#">User</a>
-			 	<ul>
-			 		<li><a href="#">Submit Research</a></li>
-			 		<li><a href="#">Edit Account</a></li>
-			 	</ul>
-			</li>
-			<li>
-  				<a href="#">About</a>
-  				<ul>
-  					<li><a href="#">Our Mission</a></li>
-  					<li><a href="#">Contact Us</a></li>
-				</ul>
-  			</li>
-  		</ul>
-  	</div>
-
-*/
