090f2020d457ece65cf5d6ec6a8bac837e700cd3
Jesse Smith <jesse.etan.smith@gmail.com>
2015-07-29 15:20:41 -0400

    connected nodeserver to mongolab database and allowed for getUser request

--- nodeserver.js
+++ nodeserver.js
@@ -1,5 +1,7 @@
-var http = require("http");
-var mongojs = require("mongojs");
+var http = require("http"),
+	mongojs = require("mongojs"),
+	fs = require("fs"),
+	url = require("url");
 
 var server = http.createServer(requestHandler);
 server.listen(8888);
@@ -8,6 +10,26 @@ var uri = "mongodb://<dbuser>:<dbpassword>@ds036698.mongolab.com:36698/alirodata
 var db = mongojs.connect(uri, ["Papers", "Users"]);
 
 function requestHandler(request, response) {
-	response.writeHead(200, {"Content-Type": "text/html"});
-	
+	//request for user is .../getUser/<firstname>/<lastname>/<school>
+	var path = url.parse(request.url).pathname;
+	var details = path.split('/');
+	if(details.indexOf("getUser") != -1) {
+		console.log("recieved request for user");
+
+		var user = db."Users".find({"firstname": details[details.indexOf("getUser") + 1],
+	                            	"lastname": details[details.indexOf("getUser") + 2],
+	                            	"school": details[details.indexOf("getUser") + 3]});
+		user = user.toArray[0];
+		response.writeHead(200, {"Content-Type": "application/json"});
+		response.write(user);
+	}
+	else {
+		fs.readFile("./index.html", function(err, file) {
+			if(err) {
+				return;
+			}
+			response.writeHead(200, {"Content-Type": "text/html"});
+			response.end(file, "utf-8");
+		});
+	}
 }
