e954c3d8d00c4729e60368ac92d16b2bc221565a
Jesse Smith <jesse.etan.smith@gmail.com>
2015-07-29 11:39:02 -0400

    put siso logic in jquery document.ready

--- javascript/index-logic.js
+++ javascript/index-logic.js
@@ -1,4 +1,19 @@
 $(document).ready(function() {
+	var signedin = false;
+	var hasAccount = false;
+	if(hasAccount == true) {
+   		if(signedin == false) {
+     			$('#si/so').innerHTML = "Sign In"  
+    	}
+    	else {
+      			$('#si/so').innerHTML = "Sign Out"
+   		}
+   	}
+	else if(hasAccount == false) {
+    	$('#si/so').href = "register.html"
+    	$('#si/so').innerHTML = "Sign Up"
+	}
+
 	$('.sm').smartmenus( {
 		showFunction: function($ul, complete) {
 			$ul.slideDown(250,complete);
@@ -10,24 +25,6 @@ $(document).ready(function() {
 	var papers=['The Effect of Music on Memory', 'Music on Time Perception', 'Productivity and Music', 'Not What You Think: Music and Memory', 'Cosmic Radiation and Ice: The Space Miracle'];
 	$('#searchBox').autocomplete({source:papers});
 }); 
-
-var signedin = false;
-var hasAccount = false;
-
-function initSiso() {
-	if(hasAccount == true) {
-    	if(signedin == false) {
-       		document.('#si/so').innerHTML = "Sign In"  
-    	}
-    	else {
-       		document.('#si/so').innerHTML = "Sign Out"
-    	}
-	}
-	else if(hasAccount == false) {
-       	document.('#si/so').href = "register.html"
-    	document.('#si/so').innerHTML = "Register Account"
-	}
-}
 
 function sisoClick() {
     if(hasAccount == true) {
