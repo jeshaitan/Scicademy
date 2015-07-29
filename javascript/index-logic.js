71df71002a7e9ec52a2a2b0325f9aaad86262488
Jesse Smith <jesse.etan.smith@gmail.com>
2015-07-29 11:46:55 -0400

    fixing jquery index-logic

--- javascript/index-logic.js
+++ javascript/index-logic.js
@@ -1,6 +1,18 @@
+var signedin = false;
+var hasAccount = false;
+
 $(document).ready(function() {
-	var signedin = false;
-	var hasAccount = false;
+	$('.sm').smartmenus( {
+		showFunction: function($ul, complete) {
+			$ul.slideDown(250,complete);
+		},
+		hideFunction: function($ul, complete) {
+		$ul.slideUp(250, complete);
+		}
+	}); 
+	var papers=['The Effect of Music on Memory', 'Music on Time Perception', 'Productivity and Music', 'Not What You Think: Music and Memory', 'Cosmic Radiation and Ice: The Space Miracle'];
+	$('#searchBox').autocomplete({source:papers});
+
 	if(hasAccount == true) {
    		if(signedin == false) {
      		$('#si/so').innerHTML = "Sign In";  
@@ -13,17 +25,6 @@ $(document).ready(function() {
     	$('#si/so').href = "register.html";
     	$('#si/so').innerHTML = "Sign Up";
 	}
-
-	$('.sm').smartmenus( {
-		showFunction: function($ul, complete) {
-			$ul.slideDown(250,complete);
-		},
-		hideFunction: function($ul, complete) {
-		$ul.slideUp(250, complete);
-		}
-	}); 
-	var papers=['The Effect of Music on Memory', 'Music on Time Perception', 'Productivity and Music', 'Not What You Think: Music and Memory', 'Cosmic Radiation and Ice: The Space Miracle'];
-	$('#searchBox').autocomplete({source:papers});
 }); 
 
 function sisoClick() {
