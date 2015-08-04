2e6632aea08ab237b7700bf945e526cea79ddbae
riddlemasta <abagh0703@gmail.com>
2015-08-04 01:52:03 -0400

    Tried changing modal to false

--- javascript/index-logic.js
+++ javascript/index-logic.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
 	$('#searchBox').autocomplete({source:papers});
 
 	$('#signInBox').dialog({
-		modal: true,
+		modal: false,
 		draggable:true,
 		resizable:false,
 		minWidth: 400,
