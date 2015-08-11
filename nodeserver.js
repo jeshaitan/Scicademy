178ae23b4a44b2ff566fbbccba2dee6d5e3d8d4d
Jesse Smith <jesse.etan.smith@gmail.com>
2015-08-11 12:43:55 -0400

    added CORS module and updated node server to return JSON object of first matching user

--- nodeserver.js
+++ nodeserver.js
@@ -1,33 +1,23 @@
 var http = require("http"),
 	mongojs = require("mongojs"),
+	express = require('express'),
+  	cors = require('cors'),
 	fs = require("fs"),
 	url = require("url");
 
-var server = http.createServer(requestHandler);
-server.listen(8888);
+app = express();
+app.use(cors());
 
 var uri = "mongodb://<dbuser>:<dbpassword>@ds036698.mongolab.com:36698/alirodatabase";
 var db = mongojs(uri, ["Papers", "Users"]);
-console.log("node server running back end of app");
-function requestHandler(request, response) {
-	//request for user is .../getUser/<username>/<password>
-	var path = url.parse(request.url).pathname;
-	var details = path.split('/');
-	if(details.indexOf("getUser") != -1) {
-		console.log("recieved request for user");
-		var user = db.Users.find({"email": details[details.indexOf("getUser") + 1],
-	                              "password": details[details.indexOf("getUser") + 2]});
-		user = user.toArray[0];
-		response.writeHead(200, {"Content-Type": "text/json"});
-		response.write(JSON.stringify(user));
-	}
-	else {
-		fs.readFile("./index.html", function(err, file) {
-			if(err) {
-				return;
-			}
-			response.writeHead(200, {"Content-Type": "text/html"});
-			response.end(file, "utf-8");
-		});
-	}
-}
+
+app.get('/getUser/:email/:passwd', function(req, res, next) {
+  var users = db.Users.find({"email": req.params.email,
+	                        "password": req.params.passwd});
+  user = users.toArray[0];
+  res.json(user);
+});
+
+app.listen(8888, function() {
+  console.log('CORS-enabled web server listening on port 8888');
+});
