5a3ca9002fcc129d38e05f4135b2d404f0b02446
riddlemasta <abagh0703@gmail.com>
2015-10-25 17:54:01 -0400

    Added google analytics, optimized for sitemap

--- public/javascript/index-logic.js
+++ public/javascript/index-logic.js
@@ -92,6 +92,7 @@ $(document).ready(function() {
 		</div>\
 		</div>';
 	var currentLink = window.location.href;
+	
 	if (currentUser == null && (currentLink.indexOf('submit.html') != -1 || currentLink.indexOf('poster.html') != -1)){
 		signInHtml = signInHtml + '<center><div id="alerttext"><p id="alerttext" style="display: inline-block">You must have an account to submit a paper.&nbsp;&nbsp;</p><a href="index.html" style="z-index: 99999">Return to home page</a></div></center>' +signInHtmlEnd;
 	}
