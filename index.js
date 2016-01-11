a3823d076253983952c1c3d4cf985fcd5dfc277d
riddlemasta <abagh0703@gmail.com>
2016-01-11 17:11:40 -0500

    almost finished author autocomplete, chagned toc

--- index.js
+++ index.js
@@ -10,27 +10,30 @@ var http = require("http"),
     mongo = require('mongodb'),
     busboy = require('connect-busboy'),
     aws = require('aws-sdk'),
-    ObjectID = require('mongodb').ObjectID,
-    mailgun = require('mailgun-js')({
-        apiKey: /*api_key*/ ,
-        domain: /*domain*/
-    });
+    ObjectID = require('mongodb').ObjectID;
 
 app = express();
 app.use(bodyParser.json());
 app.use(busboy());
 var uri = "mongodb://PublicIO:publicpass@ds036698.mongolab.com:36698/alirodatabase";
-var db = mongojs(uri, ["Papers", "Pdfs", "Users"], {
-    authMechanism: 'ScramSHA1'
+var db = mongojs(uri, ["Papers", "Pdfs", "Users"], {authMechanism: 'ScramSHA1'});
+
+
+app.post('/completeAuthor', function(req, res) {
+    //TODO search each word in the query individually and return all results ('rubin smith' currently doesn't bring up jesse smith)
+    var m = req.body.letters.term;
+    var searchObj = {"lowerName": {$regex: req.body.letters.term.toLowerCase()}};
+    db.Users.find(searchObj, function(err, doc) {
+        res.send(doc);
+    })
 });
 
 app.post('/getUser', function(req, res) {
     var searchObj = {};
-    if (req.body.hasOwnProperty('searchType') && req.body.searchType == "id") {
-        searchObj = {
-            "_id": ObjectID(req.body.query)
-        };
-    } else {
+    if(req.body.hasOwnProperty('searchType') && req.body.searchType == "id") {
+        searchObj = {"_id" : ObjectID(req.body.query)};
+    }
+    else {
         searchObj = {
             "email": req.body.email,
             "password": req.body.password
@@ -39,13 +42,12 @@ app.post('/getUser', function(req, res) {
     var user = db.Users.findOne(searchObj, function(err, doc) {
         if (err) {
             console.log(err);
-        } else {
-            if (req.body.hasOwnProperty("meta")) {
-                res.send({
-                    "doc": doc,
-                    "meta": req.body.meta
-                });
-            } else {
+        }
+        else {
+            if(req.body.hasOwnProperty("meta")) {
+                res.send({"doc": doc, "meta": req.body.meta});
+            }
+            else {
                 res.send(doc);
             }
         }
@@ -53,83 +55,59 @@ app.post('/getUser', function(req, res) {
 });
 
 app.post('/getPaper', function(req, res) {
-    if (req.body.searchType == "All") {
-        var searchObject = {
-            $or: [{
-                $text: {
-                    $search: req.body.query
-                }
-            }, {
-                keywords: req.body.query
-            }, {
-                authors: req.body.query
-            }, {
-                abstract: req.body.query
-            }]
-        };
-    } else if (req.body.searchType == "Title") {
-        var searchObject = {
-            $text: {
-                $search: req.body.query
-            }
-        };
-    } else if (req.body.searchType == "Keywords") {
-        var searchObject = {
-            keywords: req.body.query
-        };
-    } else if (req.body.searchType == "Abstract") {
-        var searchObject = {
-            abstract: req.body.query
-        };
-    } else if (req.body.searchType == "Author") {
-        db.Users.find({
-            $text: {
-                $search: req.body.query
-            }
-        }, function(err, curs) {
-            if (err) {
+    if(req.body.searchType == "All") {
+        var searchObject = {$or: [{$text: {$search: req.body.query}},
+            {keywords: req.body.query},
+            {authors: req.body.query}]};
+    }
+    else if(req.body.searchType == "Title") {
+        var searchObject = {$text: {$search: req.body.query}};
+    }
+    else if(req.body.searchType == "Keywords") {
+        var searchObject = {keywords: req.body.query};
+    }
+    else if(req.body.searchType == "Author") {
+        db.Users.find({$text: {$search: req.body.query}}, function(err, curs) {
+            if(err) {
                 console.log(err)
-            } else {
+            }
+            else {
                 var searchObject = "";
-                for (var i = 0; i < curs.length; i++) {
+                for(var i = 0; i < curs.length; i++) {
                     searchObject += "{\"authors\": \"" + curs[i]._id + "\"},"
                 }
-                searchObjectArray = JSON.parse("[" + searchObject.substring(0, searchObject.length - 1) + "]");
-                if (searchObjectArray.length == 0) {
+                searchObjectArray = JSON.parse("[" + searchObject.substring(0, searchObject.length - 1) +"]");
+                if(searchObjectArray.length == 0) {
                     res.send([]);
-                } else {
-                    db.Papers.find({
-                        $or: searchObjectArray
-                    }, function(err, curs) {
-                        if (err) {
+                }
+                else {
+                    db.Papers.find({$or: searchObjectArray}, function(err, curs) {
+                        if(err) {
                             console.log(err);
-                        } else {
+                        }
+                        else {
                             res.send(curs);
                         }
                     });
                 }
             }
         });
-    } else if (req.body.searchType == 'id') {
-        var searchObject = {
-            "_id": ObjectID(req.body.query)
-        };
-    } else if (req.body.searchType == 'every') {
+    }
+    else if(req.body.searchType == 'id') {
+        var searchObject = {"_id" : ObjectID(req.body.query)};
+    }
+    else if(req.body.searchType == 'every') {
         var searchObject = {};
     }
-    if (req.body.searchType == 'Browse') {
-        var searchObject = {
-            $query: {},
-            $orderby: {
-                $natural: -1
-            }
-        };
+    if(req.body.searchType == 'Browse') {
+        var searchObject = {$query: {}, $orderby: {$natural : -1}};
     }
-    if (req.body.searchType != "Author") {
+    if(req.body.searchType != "Author") {
         db.Papers.find(searchObject, function(err, curs) {
-            if (err) {
+            if(err) {
                 console.log(err);
-            } else {
+            }
+            else {
                 res.send(curs);
             }
         });
@@ -137,45 +115,44 @@ app.post('/getPaper', function(req, res) {
 });
 
 app.post('/getSchools', function(req, res) {
-    db.Users.find({}, {
-        school: 1
-    }, function(err, curs) {
-        if (err) {
+    db.Users.find({}, {school : 1}, function(err, curs) {
+        if(err) {
             console.log(err);
-        } else {
+        }
+        else {
             res.send(curs);
         }
     });
 });
 
 app.post('/addUser', function(req, res) {
-    db.Users.findOne({
-        "email": req.body.eml
-    }, function(err, doc) {
+    db.Users.findOne({"email" : req.body.eml}, function(err, doc) {
         if (err) {
             console.log(err);
-        } else {
-            if (!doc) {
-                db.Users.insert({
-                    email: req.body.eml,
+        }
+        else {
+            if(!doc) {
+                var lowerName = req.body.fnm + " " + req.body.lnm;
+                db.Users.insert({email: req.body.eml,
                     password: req.body.pwd,
                     name: req.body.fnm + " " + req.body.lnm,
+                    lowerName: lowerName.toLowerCase(),
                     firstname: req.body.fnm,
                     lastname: req.body.lnm,
                     school: req.body.shl,
                     grade: req.body.grd,
                     isSummer: req.body.isSum,
                     publications: [],
-                    datejoined: req.body.dte
-                }, function(err, record) {
-                    if (err) {
+                    datejoined: req.body.dte}, function(err, record) {
+                    if(err) {
                         console.log(err);
-                    } else {
+                    }
+                    else {
                         res.send(record);
-                        /* add mailer code here */
                     }
                 });
-            } else {
+            }
+            else {
                 res.send({});
             }
         }
@@ -183,45 +160,34 @@ app.post('/addUser', function(req, res) {
 });
 
 app.post('/addPaper', function(req, res) {
-    db.Papers.insert({
-        title: req.body.title,
+    db.Papers.insert({title:req.body.title,
         authors: req.body.authors,
         abstract: req.body.abstract,
         keywords: req.body.keywords,
         institution: req.body.institution,
         pdf: req.body.pdf,
         date: req.body.date,
-        published: "false"
-    }, function(err, record) {
-        if (err) {
+        published: "false"}, function(err, record) {
+        if(err) {
             console.log(err);
-        } else {
-            db.Papers.findOne({
-                abstract: req.body.abstract
-            }, function(err, doc) {
-                if (err) {
+        }
+        else {
+            db.Papers.findOne({abstract: req.body.abstract}, function(err, doc) {
+                if(err) {
                     console.log(err);
-                } else {
+                }
+                else {
                     var authorsIdStrings = doc.authors,
                         authorsOId = [];
-                    for (var i = 0; i < authorsIdStrings.length; i++) {
+                    for(var i = 0; i < authorsIdStrings.length; i++) {
                         authorsOId.push(ObjectID(authorsIdStrings[i]));
                     }
 
-                    db.Users.update({
-                        _id: {
-                            $in: authorsOId
-                        }
-                    }, {
-                        $push: {
-                            publications: ("" + doc._id)
-                        }
-                    }, {
-                        multi: true
-                    }, function(err, result) {
-                        if (err) {
+                    db.Users.update({_id: {$in: authorsOId}}, {$push: {publications: (""+doc._id)}}, {multi:true}, function(err, result) {
+                        if(err) {
                             console.log(err);
-                        } else {
+                        }
+                        else {
                             //console.log(JSON.stringify(doc.authors));
                         }
                     });
@@ -245,12 +211,8 @@ app.post('/addPdf', function(req, res) {
         fstream = fs.createWriteStream(__dirname + '/public/uploads/' + name);
         file.pipe(fstream);
         fstream.on('close', function() {
-            fs.readFile(__dirname + '/public/uploads/' + name, function(err, data) {
-                var s3bucket = new aws.S3({
-                    params: {
-                        Bucket: 'aliro-pdf-assets'
-                    }
-                });
+            fs.readFile(__dirname + '/public/uploads/' + name, function(err, data){
+                var s3bucket = new aws.S3({params: {Bucket: 'aliro-pdf-assets'}});
                 s3bucket.createBucket(function() {
                     var params = {
                         Key: name,
@@ -259,7 +221,8 @@ app.post('/addPdf', function(req, res) {
                     s3bucket.upload(params, function(err, data) {
                         if (err) {
                             console.log(err);
-                        } else {
+                        }
+                        else {
                             res.send(name);
                             fs.unlink(__dirname + '/public/uploads/' + name);
                         }
@@ -270,16 +233,13 @@ app.post('/addPdf', function(req, res) {
     });
 });
 
-function randomInt(low, high) {
+function randomInt (low, high) {
     return Math.floor(Math.random() * (high - low) + low);
 }
 
 app.post('/getPdf', function(req, res) {
     var s3 = new aws.S3();
-    var params = {
-        Bucket: 'aliro-pdf-assets',
-        Key: req.body.query
-    };
+    var params = {Bucket: 'aliro-pdf-assets', Key: req.body.query};
     var file = require('fs').createWriteStream(__dirname + '/public/uploads/' + req.body.query);
     s3.getObject(params).createReadStream().pipe(file);
 });
@@ -290,7 +250,7 @@ app.post('/clearPdf', function(req, res) {
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('*', function(req, res) {
+app.get('*', function(req, res){
     res.sendFile(path.join(__dirname + '/public/404.html'));
 });
 
