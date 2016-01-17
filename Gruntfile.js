c32db4bfe6086a4e80dad65c58a3a9049866133f
Jesse Smith <jesse.etan.smith@gmail.com>
2016-01-17 17:41:19 -0500

    redownloaded some libs, installed uglifyJS for future use

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         jsbeautifier: {
-            files: ["index.js", "Gruntfile.js", "public/**/*.js", "public/**/*.html", "public/**/*.css", "!public/css/bootstrap.min.css", "!public/css/font-awesome.min.css"],
+            files: ["index.js", "Gruntfile.js", "public/**/*.js", "public/**/*.html", "public/**/*.css", "!public/libs", "!public/css/bootstrap.min.css", "!public/css/font-awesome.min.css"],
             options: {}
         }
     });
