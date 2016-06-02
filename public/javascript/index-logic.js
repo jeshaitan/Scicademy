006ec4fd7cb0066518fddcf601910bb78ec930d2
Aram Baghdassarian <abagh0703@gmail.com>
2016-06-02 07:55:31 -0400

    Updated locations

--- public/javascript/index-logic.js
+++ public/javascript/index-logic.js
@@ -245,7 +245,7 @@ $(document).ready(function() {
 
     //REMOVE THIS IF STATEMENT TO ALLOW FOR MONETARY AWARDS PAGE
     $.get("http://ipinfo.io", function (response) {
-        if (response.city.indexOf('New Hyde') != -1 || response.city.indexOf('Manhasset') != -1 || response.city.indexOf('Great Neck') != -1) { //if they are in these locations, then don't show the awards
+        if (response.city.indexOf('New Hyde') != -1 || response.city.indexOf('Merrick') != -1 || response.city.indexOf('Manhasset') != -1 || response.city.indexOf('Great Neck') != -1) { //if they are in these locations, then don't show the awards
             bodyText = bodyText.replace('<li><a href="awards.html">Monetary Awards</a></li>', '');
             $('body').html(bodyText);
         }
